6175adf5282259e66b5a821b4a4694ad0a53f112
SiddidMandi <siddid.mandi@gmail.com>
2023-05-19 17:25:21 +0800

    first

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './shared/card';
+import EntryCard from './shared/entryCard';
+import MiniCard from './shared/miniCard';
+import Button from './shared/button';
+
+const getFonts = () => {
+  return Font.loadAsync({
+    mochiyBold: require("./newassets/fonts/MochiyPopPOne-Regular.ttf"),
+    dongleRegular: require("./newassets/fonts/Dongle-Regular.ttf"),
+    dongleBold: require("./newassets/fonts/Dongle-Bold.ttf"),
+  });
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	<EntryCard>
+		<Text>Open up App.js to start working on your app!</Text>
+	</EntryCard>
   );
 }
 
---
+++ test.js
@@ -0,0 +1,5 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const testText = "Hello from test.js!";
+export default testText;
